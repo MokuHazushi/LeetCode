b69399c98a472897d69d174925034f5e9afa600f
MokuHazushi <night.moriak@gmail.com>
2022-04-09 21:00:14 +0900

    Add simple solution

---
+++ richest_customer_wealth/src/main.rs
@@ -0,0 +1,26 @@
+use std::vec;
+
+fn main() {
+    let accounts: Vec<Vec<i32>> = vec![
+        vec![1,1,1,1],
+        vec![1,1,1,2],
+        vec![2,1,1,1]
+    ];
+
+    println!("Maximum wealth is {}", maximum_wealth(accounts));
+}
+
+fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+    let mut result = 0;
+    for user in accounts {
+        let mut maximum = 0;
+        for account in user {
+            maximum += account;
+        }
+
+        if maximum > result {
+            result = maximum;
+        }
+    }
+    result
+}
