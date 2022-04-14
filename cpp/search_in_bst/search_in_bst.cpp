a7a855cea2b763dde1327b5ef7d7e455df4440d8
MokuHazushi <night.moriak@gmail.com>
2022-04-14 09:48:24 +0900

    Add compact solution

--- cpp/search_in_bst/search_in_bst.cpp
+++ cpp/search_in_bst/search_in_bst.cpp
@@ -4,14 +4,8 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        while (root != nullptr) {
-			if (root->val == val)
-				return root;
-			else if (val < root->val)
-				root = root->left;
-			else
-				root = root->right;
-		}
+        while (root != nullptr && root->val != val)
+			root = val < root->val ? root->left : root->right;
 		return root;
     }
 };
