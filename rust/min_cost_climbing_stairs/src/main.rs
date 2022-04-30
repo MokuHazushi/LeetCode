cbb7fc8464c81aa2392e3dfebfc03b1017e25aa5
MokuHazushi <night.moriak@gmail.com>
2022-04-30 23:22:03 +0900

    Improve code syntax

--- rust/min_cost_climbing_stairs/src/main.rs
+++ rust/min_cost_climbing_stairs/src/main.rs
@@ -7,7 +7,7 @@ use std::cmp;
 impl Solution {
     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
         let mut memory = vec![-1; cost.len()];
-        
+
         if cost.len() == 2 {
             return cmp::min(cost[0], cost[1]);
         }
@@ -15,7 +15,6 @@ impl Solution {
         memory[cost.len()-1] = cost[cost.len()-1];
         memory[cost.len()-2] = cost[cost.len()-2];
         for i in (0..=cost.len()-3).rev() {
-
             memory[i] = cost[i] + cmp::min(memory[i+1], memory[i+2]);
         }
 
