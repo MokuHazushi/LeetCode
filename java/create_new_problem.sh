3e90abb0bef0686e81f88e32e78404eace92a50f
MokuHazushi <night.moriak@gmail.com>
2022-05-09 16:45:13 +0900

    Update create new problem script for java

---
+++ java/create_new_problem.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mkdir $1
+
+# Source file
+echo "class Main {" > $1/$1.java
+echo -e "\tpublic static void main(String args[]) {" >> $1/$1.java
+echo -e "\t\tSystem.out.println(\"Stuff\");" >> $1/$1.java
+echo -e "\t\tSolution solution = new Solution();" >> $1/$1.java
+echo -e "\t}" >> $1/$1.java
+echo -e "}" >> $1/$1.java
+
+# Makefile
+echo "all: $1.java" > $1/Makefile
+echo  -e "\tjavac -g $^" >> $1/Makefile
