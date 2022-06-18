f0c9f276b1ea157f5489a75beaad7eee53450541
MokuHazushi <night.moriak@gmail.com>
2022-06-18 20:58:38 +0900

    Improve solution

--- rust/maximum_score_doing_multiplication/src/main.rs
+++ rust/maximum_score_doing_multiplication/src/main.rs
@@ -6,28 +6,26 @@ use std::cmp;
 
 impl Solution {
     pub fn maximum_score(nums: Vec<i32>, multipliers: Vec<i32>) -> i32 {
-        let mut memory = vec![vec![0; nums.len()]; multipliers.len()];
-        Solution::maximum_score_dp(0, 0, &nums, &multipliers, &mut memory)   
-    }
-
-    fn maximum_score_dp(
-        i: usize, 
-        left: usize, 
-        nums: &Vec<i32>, 
-        multipliers: &Vec<i32>, 
-        memory: &mut Vec<Vec<i32>>) -> i32 {
-        if i == multipliers.len() {
-            return 0;
+        let mut memory = vec![vec![0; multipliers.len()+1]; multipliers.len()+1];
+        
+        for i in 1..=multipliers.len() {
+            for j in 0..=i {
+                if j == 0 { // right elements only
+                    memory[i][j] = memory[i-1][j] + (multipliers[i-1] * nums[nums.len()-i]);
+                    continue;
+                }
+                if j == i { // left elements only
+                    memory[i][j] = memory[i-1][j-1] + (multipliers[i-1] * nums[i-1]);
+                    continue;
+                }
+                
+                memory[i][j] = cmp::max(
+                    memory[i-1][j-1] + (multipliers[i-1] * nums[j-1]),
+                    memory[i-1][j] + (multipliers[i-1] * nums[nums.len()-i+j]))
+            }
         }
 
-        let right = nums.len() - 1 - (i - left);
-
-        if memory[i][left] == 0 {
-            memory[i][left] = cmp::max(
-                multipliers[i]*nums[left] + Solution::maximum_score_dp(i+1, left+1, nums, multipliers, memory),
-                multipliers[i]*nums[right] + Solution::maximum_score_dp(i+1, left, nums, multipliers, memory));
-        }
-        memory[i][left]
+        *memory[multipliers.len()].iter().max().unwrap()
     }
 }
 
@@ -50,6 +48,7 @@ fn main() {
     let mut test_set = Vec::new();
 
     test_set.push(Test::new(vec![1,2,3], vec![3,2,1]));
+    test_set.push(Test::new(vec![-5,-3,-3,-2,7,1], vec![-10,-5,3,4,6]));
 
     for test in test_set {
         println!("nums={:?}, multipliers={:?}", &test.nums, &test.multipliers);
