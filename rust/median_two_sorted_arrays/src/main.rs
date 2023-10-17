b31c19dc4aa22eb3ededa2819d1b6946ee876a66
MokuHazushi <mokuhazushi.programming@gmail.com>
2023-10-17 19:32:51 +0900

    Add 'median of two sorted arrays'

--- rust/median_two_sorted_arrays/src/main.rs
+++ rust/median_two_sorted_arrays/src/main.rs
@@ -21,64 +21,89 @@ impl Solution {
 
     pub fn search_median(target: &Vec<i32>, other: &Vec<i32>) -> Option<f64> {
         let (mut i, mut j) = (0 as i32, target.len() as i32);
-        let one_median = (target.len() + other.len()) % 2 == 1;
+        let odd_length = (target.len() + other.len()) % 2 == 1;
 
         while i < j {
             let mid = (i+j)/2;
             let insert_index = Solution::insert_index(other, target[mid as usize]) as i32;
 
             let nb_smaller = mid + insert_index;
-            let nb_greater = j-mid-1 + other.len() as i32-insert_index;
+            let nb_greater = target.len() as i32-mid-1 + other.len() as i32-insert_index;
 
-            if one_median && nb_greater == nb_smaller {
+            if odd_length && nb_greater == nb_smaller {
                 let mid = mid as usize;
                 return Some(target[mid] as f64);
             }
-            if !one_median && nb_smaller-nb_greater == 1 {
+            if !odd_length && (nb_smaller-nb_greater).abs() == 1 {
                 let mid = mid as usize;
                 let insert_index = insert_index as usize;
                 let mut median = target[mid];
+                let mut neighbor;
 
-                println!("target={:?}", target);
-                println!("mid={mid}, insert_index={insert_index}");
-
-                if mid == 0 {
-                    median += other[insert_index];
+                if nb_smaller > nb_greater {
+                    neighbor = i32::MIN;
+                    if insert_index > 0 {
+                        neighbor = other[insert_index-1];
+                    }
+                    if mid != 0 {
+                        neighbor = cmp::max(neighbor, target[mid-1]);
+                    }
                 }
                 else {
-                    median += cmp::max(target[mid-1], other[insert_index-1]);
+                    neighbor = i32::MAX;
+                    if insert_index < other.len() {
+                        neighbor = other[insert_index];
+                    }
+                    if mid != target.len()-1 {
+                        neighbor = cmp::min(neighbor, target[mid+1]);
+                    }
                 }
+                
+                println!("target={:?}", target);
+                println!("mid_index={mid}, insert_index={insert_index}, neighbor={neighbor}");
+
+                median += neighbor;
                 return Some((median as f64)/2.0);
             }
 
             if nb_smaller < nb_greater {
                 i = mid+1;
             }
-            j = mid;
+            else {
+                j = mid;
+            }
         }
 
         None
     }
 
     /*
-    Return the index where to insert target in nums
+    Return the left-most index where to insert target in nums.
+    If target is bigger than the last element of nums, returned
+    index will be the size of nums
      */
     pub fn insert_index(nums: &Vec<i32>, target: i32) -> usize {
         let (mut i, mut j) = (0, nums.len());
 
-        while i < j {
+        while j-i > 1 {
             let mid = (i+j)/2;
-            if target == nums[mid] {
+            if target == nums[mid] && target != nums[mid-1] {
                 return mid;
             }
-            else if target < nums[mid] {
+            else if target <= nums[mid] {
                 j = mid;
             }
             else {
-                i = mid+1;
+                i = mid;
             }
         }
-        return i;
+        if target == nums[i] {
+            return i;
+        }
+        if target > nums[i] {
+            return i+1;
+        }
+        0
     }
 
     /*
@@ -131,7 +156,15 @@ fn main() {
         vec![vec![1, 100], vec![1,2]],
         vec![vec![1,1], vec![1,1]],
         vec![vec![1,3], vec![2,7]],
-        vec![vec![1,2,2], vec![1,2,3]]];
+        vec![vec![1,2,2], vec![1,2,3]],
+        vec![vec![2,2,4,4], vec![2,2,4,4]],
+        vec![vec![2], vec![1,3,4]],
+        vec![vec![3], vec![1,3,4]],
+        vec![vec![2], vec![1,3,4,5]],
+        vec![vec![2], vec![1,3,4,5,6,7]],
+        vec![vec![1,3], vec![2,4,5,6,7,8]],
+        vec![vec![1,2,4], vec![3]],
+        vec![vec![7,8,9], vec![1,2,3,4,5,6,10]]];
 
     for test in &test_cases {
         println!("{:?} -> {}", 
