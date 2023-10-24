6eb084fb4d7a1d653754eba1c80af737b9a6e337
MokuHazushi <mokuhazushi.programming@gmail.com>
2023-10-24 13:39:35 +0900

    Add 'symmetric tree' iterative solution

--- python/symmetric_tree/symmetric_tree.py
+++ python/symmetric_tree/symmetric_tree.py
@@ -4,9 +4,10 @@ from my_collections import TreeNode
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isSymetric_rec(root.left, root.right) 
+        # return self.isSymetric_rec(root.left, root.right)
+        return self.isSymetric_it(root)
     
-    def isSymetric_rec(self, left, right) -> bool:
+    def isSymetric_rec(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
         if left == None or right == None:
             return left == right
 
@@ -14,6 +15,29 @@ class Solution:
             return False
         
         return self.isSymetric_rec(left.left, right.right) and self.isSymetric_rec(left.right, right.left)
+    
+    def isSymetric_it(self, root: Optional[TreeNode]) -> bool:
+        left, right = ([root], [root])
+
+        while left and right:
+            node_L = left.pop()
+            node_R = right.pop()
+
+            if (node_L == None or node_R == None) and (node_L != node_R):
+                return False
+            
+            if node_L == None:
+                continue
+
+            if node_L.val != node_R.val:
+                return False
+
+            left.append(node_L.left)
+            left.append(node_L.right)
+            right.append(node_R.right)
+            right.append(node_R.left)
+
+        return True
     
 tree = TreeNode(1, 
                 TreeNode(2, TreeNode(3), TreeNode(4)),
