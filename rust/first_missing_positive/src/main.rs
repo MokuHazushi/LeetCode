1ed4e2d692cc04f95609d404358090ce416ba8fc
MokuHazushi <mokuhazushi.programming@gmail.com>
2023-10-26 15:20:31 +0900

    Add 'first missing positive' solution

--- rust/first_missing_positive/src/main.rs
+++ rust/first_missing_positive/src/main.rs
@@ -1,28 +1,59 @@
+use std::cmp;
+
 struct Solution {}
 
 impl Solution {
+    /*
+    We want to use 'nums' as a hashmap where:
+    - 'nums[i] == 0' means integer 'i+1' was not in original input array
+    - 'nums[i] == -1' means integer 'i+1' was in original input array (regardless of number of occurences)
+    */
     pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        let mut ans1 = Solution::search(&nums, 1);
-        let mut ans2 = Solution::search(&nums, ans1);
+        // Clean-up input
+        let mut nums: Vec<_> = nums.iter().map(|&x| cmp::max(x, 0)).collect();
+        
+        for i in 0..nums.len() {
+            let key = nums[i];
 
-        while ans1 != ans2 {
-            ans1 = ans2;
-            ans2 = Solution::search(&nums, ans1);
-        }
-        ans1
-    }
-
-    pub fn search(nums: &Vec<i32>, start: i32) -> i32 {
-        let (mut missing, mut i) = (start, 0);
-
-        while i < nums.len() {
-            if nums[i] == missing || nums[nums.len()-1-i] == missing {
-                missing += 1;
+            // We already processed this key
+            if key <= 0 {
                 continue;
             }
-            i += 1;
+
+            nums[i] = 0; // Indicate the key is processed
+            Solution::put(&mut nums, key);
         }
-        missing
+
+        // Search for the solution in the newly populated hashmap
+        for i in 0..nums.len() {
+            if nums[i] == 0 {
+                return i as i32 + 1;
+            }
+        }
+        nums.len() as i32 + 1
+    }
+
+    fn put(map: &mut Vec<i32>, key: i32) {
+        /*
+        - 'key <= 0' means 'key' is actually an already assigned value in the map
+        - 'key > map.len()' means 'key' is bigger than the solution space
+        */
+        if key <= 0 || key > map.len() as i32 {
+            return;
+        }
+
+        let hash = (key-1) as usize;
+
+        // map[hash] can be overwritten
+        if map[hash] <= 0 {
+            map[hash] = -1;
+            return;
+        }
+
+        // map[hash] value should be handled through a recursive call
+        let next_key = map[hash];
+        map[hash] = -1;
+        Solution::put(map, next_key);
     }
 }
 
@@ -31,7 +62,11 @@ fn main() {
         vec![1,2,0],
         vec![3,4,-1,1],
         vec![7,8,9,11,12],
-        vec![7,8,9]
+        vec![7,8,9],
+        vec![2],
+        vec![2,2],
+        vec![1, 1000],
+        vec![2, 1]
     ];
 
     for test in &test_cases {
